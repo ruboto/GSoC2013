b7dc05934f5d838fa947f42a617392b7402c0987
Nyangawa <ryua775@gmail.com>
2013-07-09 02:39:08 +0800

    update draw script

--- calender/jruby_loading_logs/cairo.rb
+++ calender/jruby_loading_logs/cairo.rb
@@ -3,8 +3,8 @@ include Cairo
 benchmark = {}
 #LOGFILE = "Ruby1_8-off.log"
 #LOGFILE = "Ruby1_8-offir.log"
-LOGFILE = "Ruby1_9-off.log"
-#LOGFILE = "Ruby1_9-offir.log"
+#LOGFILE = "Ruby1_9-off.log"
+LOGFILE = "Ruby1_9-offir.log"
 
 File.open(LOGFILE).each_line do |line|
   benchmark.store *(line.split " ")
@@ -25,6 +25,7 @@ context.fill do
 end
 
 benchmark.each do |name, data|
+  next if name =~ /\w+\/kernel19\/\w+/
   context.set_source_rgb 0,0,1
   context.rectangle start, height, step, -data.to_f/3
   context.fill
